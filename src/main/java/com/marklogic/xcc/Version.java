ffe7f16093bdded952bf7bf3d1a85ca7115ed481
Rob Rudin <rjrudin@gmail.com>
2016-01-01 13:41:57 -0500

    Stubbing out a Version class

---
+++ src/main/java/com/marklogic/xcc/Version.java
@@ -0,0 +1,20 @@
+package com.marklogic.xcc;
+
+public class Version {
+
+    public static int getVersionMajor() {
+        return 8;
+    }
+
+    public static int getVersionMinor() {
+        return 0;
+    }
+
+    public static int getVersionPatch() {
+        return 4;
+    }
+
+    public static String getVersionString() {
+        return "8.0.4";
+    }
+}
